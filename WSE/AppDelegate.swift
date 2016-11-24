1847393519fd6853e0cda1b12171ce55ff65c16b
abdulkarim <ali@abdulkarims-iMac.local>
2016-11-24 07:37:58 +0300

    Initial Commit

---
+++ WSE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WSE
+//
+//  Created by abdulkarim on 11/24/16.
+//  Copyright © 2016 abdulkarim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WSE/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WSE
+//
+//  Created by abdulkarim on 11/24/16.
+//  Copyright © 2016 abdulkarim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
